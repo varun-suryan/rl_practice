f27d0b17d3a17364bab568fc0e2c1c01fb24f104
varun <varunsuryan9@gmail.com>
2017-09-25 18:21:42 -0400

    raq_qlearn

---
+++ main_q.py
@@ -0,0 +1,45 @@
+import random
+
+
+class Model:
+    def __init__(self, actions=[0,1,2,3], epsilon=0.05, alpha=0.4, gamma=0.9):
+        self.q = {}
+
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.actions = actions
+
+    def getQ(self, state, action):
+        return self.q.get((state, action), 0.0)
+        # return self.q.get((state, action), 1.0)
+    def getQTable(self):
+        return self.q
+
+    def learnQ(self, state, action, reward, value):
+        oldv = self.q.get((state, action))
+        if oldv is None:
+            self.q[(state, action)] = reward
+        else:
+            self.q[(state, action)] = oldv + self.alpha * (value - oldv)
+
+    def chooseAction(self, state):
+        if random.random() < self.epsilon:
+            action = random.choice(self.actions)
+        else:
+            q = [self.getQ(state, a) for a in self.actions]
+            maxQ = max(q)
+            count = q.count(maxQ)
+            if count > 1:
+                best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+                i = random.choice(best)
+            else:
+                i = q.index(maxQ)
+
+            action = self.actions[i]
+        return action
+
+    def learn(self, state1, action1, reward, state2):
+        maxqnew = max([self.getQ(state2, a) for a in self.actions])
+        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
+
