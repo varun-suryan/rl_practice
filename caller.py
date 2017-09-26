0911896209f42afa540c9d8a784bb0919f264797
varun <varunsuryan9@gmail.com>
2017-09-25 20:00:17 -0400

    full Q-learning in one code

---
+++ caller.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import planner
+# updateObj = planner.gprmax()
+
+GRID = 1
+
+states = [(x,y) for x in range(-GRID, GRID + 1) for y in range(-GRID, GRID + 1)]
+actions=[(1,0), (-1,0), (0,1), (0,-1)]
+T = {}
+
+#T = {(s, a): [(s[0] + a[0], s[1] + a[1]), 1]}
+
+for s in states:
+	for a in actions:
+		temporary = {(s, a): [(( max(min(s[0] + a[0], GRID), -GRID) , max(min(s[1] + a[1], GRID), -GRID)), 1)]}
+		T = dict(T.items() + temporary.items() )
+
+print T
+
+U = updateObj.value_iteration ( T , states, GRID)
+policy = updateObj.best_policy(U, T, states, GRID)
+
+
+for x in xrange(-GRID, GRID + 1):
+	for y in xrange(-GRID, GRID + 1):
+		a, b = policy[x, y]
+		plt.quiver(x, y, a, b)
+
+plt.xlim(-GRID - 1, GRID + 1)
+plt.ylim(-GRID - 1, GRID + 1)
+plt.show()
--- main_q.py
+++ main_q.py
@@ -1,45 +1,75 @@
 import random
+from time import sleep
+import matplotlib.pyplot as plt
+
+epsilon = 0.02
+alpha = 0.2
+gamma = 0.9
+GRID = 2
+num_of_episodes = 30
+q = {}
+
+states = [(x,y) for x in range(-GRID, GRID + 1) for y in range(-GRID, GRID + 1)]
+actions=[(1,0), (-1,0), (0,1), (0,-1)]
 
 
-class Model:
-    def __init__(self, actions=[0,1,2,3], epsilon=0.05, alpha=0.4, gamma=0.9):
-        self.q = {}
 
-        self.epsilon = epsilon
-        self.alpha = alpha
-        self.gamma = gamma
-        self.actions = actions
+term_state = (GRID, GRID)
 
-    def getQ(self, state, action):
-        return self.q.get((state, action), 0.0)
-        # return self.q.get((state, action), 1.0)
-    def getQTable(self):
-        return self.q
+def reward_dynamics(state):
+    if state == term_state:
+        return 100
+    else:
+        return -10
 
-    def learnQ(self, state, action, reward, value):
-        oldv = self.q.get((state, action))
-        if oldv is None:
-            self.q[(state, action)] = reward
+def getQ(state, action):
+    return q.get((state, action), 1000.0)
+    # return self.q.get((state, action), 1.0)
+
+def learnQ(state, action, reward, value):
+    oldv = q.get((state, action))
+    if oldv is None:
+        q[(state, action)] = 1000 + alpha * (value - 1000)
+    else:
+        q[(state, action)] = oldv + alpha * (value - oldv)
+
+def chooseAction(state):
+    if random.random() < epsilon:
+        action = random.choice(actions)
+    else:
+        q = [getQ(state, a) for a in actions]
+        maxQ = max(q)
+        count = q.count(maxQ)
+        if count > 1:
+            best = [i for i in range(len(actions)) if q[i] == maxQ]
+            i = random.choice(best)
         else:
-            self.q[(state, action)] = oldv + self.alpha * (value - oldv)
+            i = q.index(maxQ)
 
-    def chooseAction(self, state):
-        if random.random() < self.epsilon:
-            action = random.choice(self.actions)
-        else:
-            q = [self.getQ(state, a) for a in self.actions]
-            maxQ = max(q)
-            count = q.count(maxQ)
-            if count > 1:
-                best = [i for i in range(len(self.actions)) if q[i] == maxQ]
-                i = random.choice(best)
-            else:
-                i = q.index(maxQ)
+        action = actions[i]
+    return action
 
-            action = self.actions[i]
-        return action
+def learn(state1, action1, reward, state2):
+    maxqnew = max([getQ(state2, a) for a in actions])
+    learnQ(state1, action1, reward, reward + gamma*maxqnew)
 
-    def learn(self, state1, action1, reward, state2):
-        maxqnew = max([self.getQ(state2, a) for a in self.actions])
-        self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
+
+for i in range(0, num_of_episodes):
+    curr_state = (-GRID, -GRID)
+    
+    while curr_state != term_state:
+        action = chooseAction(curr_state)
+        next_state = (max(min(curr_state[0] + action[0], GRID), -GRID) , max(min(curr_state[1] + action[1], GRID), -GRID))
+        learn(curr_state, action, reward_dynamics(next_state), next_state)
+        curr_state = next_state
+    print q
+    print '\n'
+
+for state in states:
+    final_action = chooseAction(state)
+    plt.quiver(state[0], state[1], final_action[0], final_action[1])
+
+plt.xlim(-GRID - 1, GRID + 1)
+plt.ylim(-GRID - 1, GRID + 1)
+plt.show()
 
