f35823cd4bcdcd1d5d21e07caf53e76a22fddc81
Varun Suryan <varunsuryan9@gmail.com>
2019-04-09 17:25:20 -0400

    q_learning on Taxi-v2

--- gym-project.py
+++ gym-project.py
@@ -4,7 +4,7 @@ import random
 import matplotlib.pyplot as plt
 
 env = gym.make('Taxi-v2')
-q_table = np.zeros([env.observation_space.n, env.action_space.n])
+Q = np.zeros([env.observation_space.n, env.action_space.n])
 learning_rate = 0.9
 discount = 0.9
 epsilon = 0.1
@@ -15,12 +15,12 @@ for episode in range(0, num_episode):
 	state = env.reset()
 	done = False
 	while done != True:
-		if random.random() >= epsilon: curr_action = np.argmax(q_table[state, :])
+		if random.random() >= epsilon: curr_action = np.argmax(Q[state])
 		else: curr_action = env.action_space.sample()
 		new_state, reward, done, info = env.step(curr_action)
-		q_table[state, curr_action] += learning_rate*(reward + discount*max(q_table[new_state, :]) - q_table[state, curr_action])
+		Q[state, curr_action] += learning_rate*(reward + discount*max(Q[new_state]) - Q[state, curr_action])
 		state = new_state
 		rewards[episode,] += reward 
 
-plt.plot(rewards)
+plt.plot(rewards[::20])
 plt.show()
