e67435f34e795b29a0d73ee1a9f155dc8efd9d79
Varun Suryan <varunsuryan9@gmail.com>
2019-05-27 23:32:30 -0400

    dqn implement started

--- gym-project.py
+++ gym-project.py
@@ -3,24 +3,25 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-env = gym.make('Taxi-v2')
-Q = np.zeros([env.observation_space.n, env.action_space.n])
+env = gym.make('MsPacman-v0')
+print(env.env.action_space)
+# Q = np.zeros([env.observation_space.n, env.action_space.n])
 learning_rate = 0.9
 discount = 0.9
 epsilon = 0.1
 num_episode = 1000
 rewards = np.zeros([num_episode,])
 
-for episode in range(0, num_episode):
-	state = env.reset()
-	done = False
-	while done != True:
-		if random.random() >= epsilon: curr_action = np.argmax(Q[state])
-		else: curr_action = env.action_space.sample()
-		new_state, reward, done, info = env.step(curr_action)
-		Q[state, curr_action] += learning_rate*(reward + discount*max(Q[new_state]) - Q[state, curr_action])
-		state = new_state
-		rewards[episode,] += reward 
+# for episode in range(0, num_episode):
+# 	state = env.reset()
+# 	done = False
+# 	while done != True:
+# 		if random.random() >= epsilon: curr_action = np.argmax(Q[state])
+# 		else: curr_action = env.action_space.sample()
+# 		new_state, reward, done, info = env.step(curr_action)
+# 		Q[state, curr_action] += learning_rate*(reward + discount*max(Q[new_state]) - Q[state, curr_action])
+# 		state = new_state
+# 		rewards[episode,] += reward 
 
-plt.plot(rewards[::20])
-plt.show()
+# plt.plot(rewards[::20])
+# plt.show()
--- side_python.py
+++ side_python.py
@@ -1,2 +1,12 @@
-for _ in range(10):
-	print(2)
+class Person:
+  def __init__(self, name = 'Varun'):
+    self.name = name
+    self.age = 26
+
+  def test_func(self):
+  	print('My name is ' + self.name + ' and I am ' + str(self.age) + ' years old.')   
+  
+
+p1 = Person()
+
+p1.test_func()
